bc39a4eb6d94661deaf5a33677dea06446d83e31
ashvilohiya <alohiya@purdue.edu>
2017-03-15 22:40:43 -0400

    Updating the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,11 +8,27 @@ var React = require('react');
 //   }
 // });
 
+
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application. Search a city in the search bar to get the current temperature in degree celsius
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+        <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used
+        </li>
+        <li>
+        <a href="http://openweathermap.org">Open Weather Map</a> - This was the service used to fetch the data
+        </li>
+        <li>
+        <a href="https://github.com/ashvinlohiya/ReactWeather"> Source Code </a> - You can find the source code here!
+        </li>
+      </ul>
     </div>
   )
 };
